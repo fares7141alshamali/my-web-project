2476b27f129c5d7f77de91d262f2bca66580b488
fares7141alshamali <fares.alshamali@jo.zain.com>
2026-02-17 16:32:19 +0300

    Add s function to first.py

--- first.py
+++ first.py
@@ -14,6 +14,10 @@ def about():
 def contacts():
     return render_template("contacts.html")
 
+@app.route('/sss')
+def contacts():
+    return render_template("sss.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
