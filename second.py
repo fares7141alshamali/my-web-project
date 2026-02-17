7168c3d0c179074a34f7d66fc028ef08bdfbbebd
fares7141alshamali <fares.alshamali@jo.zain.com>
2026-02-17 16:39:38 +0300

    Create second.py

---
+++ second.py
@@ -0,0 +1,23 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("Hi 1.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/contacts')
+def contacts():
+    return render_template("contacts.html")
+
+@app.route('/sss')
+def contacts():
+    return render_template("sss.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
