994189c9f6d7e0cc64c3ad7865404c6a05207eef
fares7141alshamali <fares.alshamali@jo.zain.com>
2026-02-17 16:30:01 +0300

    Add contacts function to first.py

--- first.py
+++ first.py
@@ -10,6 +10,10 @@ def home():
 def about():
     return render_template("about.html")
 
+@app.route('/contacts')
+def contacts():
+    return render_template("contacts.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
